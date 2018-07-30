fb2a3f7e4287bf8f89a9560941cdb4a29a52c67a
Emily Asch <e.asch@me.com>
2018-07-30 19:10:21 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,10 +18,7 @@ function whileLoop(number){
 
 
 function doWhileLoop(array){
-  do {array.pop()}
-  while(array.length > 0 || maybeTrue())
-}
-function maybeTrue() {
-  return Math.random() >= 0.5
-}
 
+  
+  
+}
