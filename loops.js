774ec010f7baa217d127c56f932b14d82b72ef4a
Emily Asch <e.asch@me.com>
2018-07-30 18:27:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,9 @@
-for (let i = 0; i < 25; i++){
+function forLoop (array){
+  for (let i = 0; i < 25; i++){
   if (i === 1){
     console.log ("I am 1 strange loop.")
   } else { (i === 0)
     console.log("I am ${i} strange loops.")
   }
+}
 }
